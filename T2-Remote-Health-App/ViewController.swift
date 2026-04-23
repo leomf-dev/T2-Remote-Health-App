824e019e17b197c04cfd8dd2b634387c336b66d9
Xio05 <kimi_kimi123x@hotmail.com>
2026-04-23 15:09:07 -0500

    Initial Commit

---
+++ T2-Remote-Health-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  T2-Remote-Health-App
+//
+//  Created by DESIGN on 23/04/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
